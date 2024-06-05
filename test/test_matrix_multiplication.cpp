aeb2e314945aae06663a6042c9223ab92d9ad10f
AnnaPoala00 <annapaola.izzo@mail.polimi.it>
2024-06-05 12:42:34 +0200

    Added the rest of the comments

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -39,6 +39,9 @@ TEST(MatrixMultiplicationTest, TestMultiplyZeroMatrices) {
 }
 
 // Test Identity Matrices
+// Check the correctness of matrix multiplication between identity matrices. 
+// The precondition to be satisfied is the correct size of the matrices.
+// We expect an identity matrix as result of multiplication, but the final status is FAILED
 TEST(MatrixMultiplicationTest, TestMultiplyIdentityMatrices) {
   std::vector<std::vector<int>> A = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
   std::vector<std::vector<int>> B = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
@@ -52,6 +55,9 @@ TEST(MatrixMultiplicationTest, TestMultiplyIdentityMatrices) {
 }
 
 // Test Ones Matrices
+// Check the correctness of matrix multiplication between ones matrices. 
+// The precondition to be satisfied is the correct size of the matrices.
+// We expect a ones matrix multiplied by 3 as result of multiplication, but the final status is FAILED
 TEST(MatrixMultiplicationTest, TestMultiplyOnesMatrices) {
   std::vector<std::vector<int>> A = {{1, 1, 1}, {1, 1, 1}};
   std::vector<std::vector<int>> B = {{1, 1}, {1, 1}, {1, 1}};
@@ -65,6 +71,9 @@ TEST(MatrixMultiplicationTest, TestMultiplyOnesMatrices) {
 }
 
 // Test diagonal matrices multiplication
+// Add a unitary matrix x 4 to  
+// The precondition to be satisfied is the correct size of the matrices.
+// We expect a diagonal matrix as result of multiplication, and the final status is PASSED
 TEST(MatrixMultiplicationTest, TestDigonalMatrices) {
   std::vector<std::vector<int>> A = {{8, 4}, {4, 4}, {4, 4}};
   std::vector<std::vector<int>> B = {{4, 4}, {4, 4}};
@@ -78,7 +87,10 @@ TEST(MatrixMultiplicationTest, TestDigonalMatrices) {
   ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-// Test Matrices containg negative numbers
+// Test Matrices containing negative numbers
+// Check the correctness of matrix multiplication between matrices that contain negative numbers. 
+// The precondition to be satisfied is the correct size of the matrices.
+// We expect a positive matrix as result of multiplication, but the final status is FAILED
 TEST(MatrixMultiplicationTest, TestMultiplyNegativeMatrices) {
   std::vector<std::vector<int>> A = {{-1, -2, -3}, {-4, -5, -6}};
   std::vector<std::vector<int>> B = {{-7, -8}, {-9, -10}, {-11, -12}};
@@ -91,8 +103,11 @@ TEST(MatrixMultiplicationTest, TestMultiplyNegativeMatrices) {
   ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-// Test Matrices by filling them entirely with the same number ranging from 0 to
-// 100 and calculating their power 2
+// Test two differently sized Matrices by filling them entirely with the same number ranging from 0 to 100
+// Check the correctness of matrix multiplication between unitary matrices multiplied by a number ranging from 0 to 100. 
+// The precondition to be satisfied is the correct size of the matrices.
+// We expect the result matrix to be equivalent with matrix to the result of multiplication function without errors, but the final status // is FAILED equivalent 
+
 TEST(MatrixMultiplicationTest, TestMultiplySameNumberMatrices) {
   for (int i = 0; i < 100; i++) {
     std::vector<std::vector<int>> A = {{i, i, i}, {i, i, i}};
@@ -109,6 +124,10 @@ TEST(MatrixMultiplicationTest, TestMultiplySameNumberMatrices) {
 }
 
 // Test multiplication with Identity matrix
+// Check the correctness of matrix multiplication between an identity matrix and a matrix, whose elements are all the same number from 0 to 100. 
+// These values are set with a for loop. 
+// The precondition to be satisfied is the correct size of the matrices.
+// We expect the A matrix as result of multiplication, but the final status is FAILED
 TEST(MatrixMultiplicationTest, TestMultiplyWithIdentityMatrix) {
   for (int i = 0; i < 100; i++) {
     std::vector<std::vector<int>> A = {{i, i}, {i, i}};
